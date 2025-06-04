d8e60e06859f89dcd666e6fddc8e426f7fbb56b1
Miguel Flandez <immfpnvs2@gmail.com>
2025-06-04 18:43:17 +0800

    timer function
    note: when doing intervals, do not use the usestates as conditions as they are async and are not reliable, it should be reliant on a computation instead

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,14 +1,43 @@
-import { useState } from "react"
+import { useEffect, useState, useRef } from "react"
 
 export default function Pomodoro(){
 
     const [selected, setSelected] = useState('Pomodoro')
-    const [minutes, setMinutes] = useState(25)
-    const [seconds, setSeconds] = useState(0)
+    const [minutes, setMinutes] = useState(2)
+    const [seconds, setSeconds] = useState(1)
+    const [start, setStart] = useState(false)
+   
+    const timerRef = useRef(null)
+    const secondsLeft = useRef((minutes*60)+seconds)
 
+
+  useEffect(() => {
+    if (start) {
+      timerRef.current = setInterval(() => {
+        if(secondsLeft.current){
+            secondsLeft.current -= 1;
+            const m = Math.floor(secondsLeft.current/60)
+            const s = secondsLeft.current % 60
+
+            setMinutes(m)
+            setSeconds(s)
+        }else{
+            clearInterval(timerRef.current)
+            setStart(false)
+        }
+      }, 1000);
+
+      
+    }
+
+    return () => clearInterval(timerRef.current);
+  }, [start]);
+
+
+    
     return(
-        <div className="absolute left-1/2 top-1/4 -translate-x-1/2 -translate-y-1.2 rounded-md
-        bg-[#852D2D] border-0 text-white text-md font-mono p-4 flex flex-col items-center gap-8 ">   
+        <div className="absolute left-1/2 top-1/10 -translate-x-1/2 -translate-y-1.2 rounded-md
+        bg-[#852D2D] border-0 text-white text-md font-mono p-8 h-[350px] flex flex-col items-center gap-8 ">   
 
             <div id="header buttons" className="flex items-center justify-center gap-16 cursor-pointer">
                 <span className={`px-4 rounded-sm ${selected==='Pomodoro' ? 'bg-[#712626]' : '' }`} onClick={()=>setSelected('Pomodoro')}>Pomodoro</span>
@@ -19,7 +48,8 @@ export default function Pomodoro(){
         
             <span className="text-9xl font-bold">{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</span>
             
-            <button className={`bg-[#0E1011] rounded-md text-[#C85757] font-bold font-mono px-12 py-4`}>START</button>
+            <button className={`bg-[#0E1011] rounded-md text-[#C85757] font-bold font-mono px-12 py-4 ${start ? 'translate-y-1' : 'border-b-4 border-[#1A1D1F] '}`}
+            onClick={()=>setStart(prev=>!prev)}>{start?'Pause':'Start'}</button>
         </div>
     )
 }
