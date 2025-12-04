8207b08950cae7db4e946c9fa3febbe63cc1512c
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-04 18:01:42 +0800

    added responsive styles to pomodoro

--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from "react"
+import { Header } from "@/components"
 
 export default function Pomodoro(){
 
@@ -76,29 +77,34 @@ export default function Pomodoro(){
   }, [start]);
 
   useEffect(()=>{
-    document.body.style.transition = 'background-color 1s ease-out'
+    document.body.style.transition = 'background-color 0.2s ease-out'
   }),[]
 
 
     
     return(
-        <>
-          <span className="block font-bold text-4xl text-white font-mono mt-4 text-center">Cycle {pomoTracker}</span>
-          <div className={`absolute left-1/2 top-1/6 -translate-x-1/2 -translate-y-1.2 rounded-md
-          ${selected==='Pomodoro' ? 'bg-[#852D2D]' : selected==='Short' ? 'bg-[#225F63]' : 'bg-[#234F6D]'} transition-colors duration-1000 border-0 text-white text-md font-mono p-8 h-[350px] flex flex-col items-center gap-8 `}>   
+      <>
+      <Header/>
+        <div className="flex flex-col gap-[5rem] items-center ">
+          <span className="block font-bold text-[2rem] text-white font-mono mt-4 text-center">Cycle {pomoTracker}</span>
+          <div className={`${selected==='Pomodoro' ? 'bg-[#852D2D]' : selected==='Short' ? 'bg-[#225F63]' : 'bg-[#234F6D]'} 
+          rounded-md transition-colors duration-200 border-0 text-white text-md font-mono p-8 max-sm:w-[90%] flex flex-col items-center gap-[2rem]`}>   
 
-              <div id="header buttons" className="flex items-center justify-center gap-4 cursor-pointer px-4">
+              <div id="header buttons" className="flex items-center justify-center gap-[1vw] max-sm:text-xs cursor-pointer px-[1rem] whitespace-nowrap">
                   <span className={`px-4 py-2 rounded-sm ${selected==='Pomodoro' ? 'bg-[#1211112f]' : '' }`} onClick={()=>setSelected('Pomodoro')}>Pomodoro</span>
                   <span className={`px-4 py-2 rounded-sm ${selected==='Short' ? 'bg-[#1211112f]' : '' }`} onClick={()=>setSelected('Short')}>Short Break</span>
                   <span className={`px-4 py-2 rounded-sm ${selected==='Long' ? 'bg-[#1211112f]' : '' }`} onClick={()=>setSelected('Long')}>Long Break</span>
               </div>
 
           
-              <span className="text-9xl font-bold">{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</span>
+              <span className="text-[8rem] max-sm:text-[7rem] font-bold">{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</span>
+              <div id="button-container" className="h-16">
+                <button className={`bg-[#0E1011] rounded-md transition-colors duration-200 ${selected==='Pomodoro'?'text-[#C85757]' : selected==='Short' ? 'text-[#79CBD0]' : 'text-[#6EA8D0]'} font-bold font-mono px-12 py-4 ${start ? 'translate-y-1' : 'border-b-4 border-[#1A1D1F] '}`}
+                onClick={()=>setStart(prev=>!prev, clickSound.play())}>{start?'PAUSE':'START'}</button>
+              </div>
               
-              <button className={`bg-[#0E1011] rounded-md transition-colors duration ${selected==='Pomodoro'?'text-[#C85757]' : selected==='Short' ? 'text-[#79CBD0]' : 'text-[#6EA8D0]'} font-bold font-mono px-12 py-4 ${start ? 'translate-y-1' : 'border-b-4 border-[#1A1D1F] '}`}
-              onClick={()=>setStart(prev=>!prev, clickSound.play())}>{start?'PAUSE':'START'}</button>
           </div>
-        </>
+        </div>
+      </>
     )
 }
