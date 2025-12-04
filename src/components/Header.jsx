8e9ff2a968e9f4b79ae8bfe7b4428173dfb929b1
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-04 18:00:57 +0800

    added header for navigating pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
-
+import { useNavigate } from "react-router-dom"
 
 export default function Header(){
 
+const navigate = useNavigate()
+
     return(
-        <div className="flex">
-            <span>Habits</span>
-            <span>Pomodoro</span>
+        <div className="flex justify-evenly font-mono font-bold text-2xl py-[2vh] cursor-default
+        ">
+            <span className="hover:bg-[#1211112f] transition-all hover:-translate-y-1 py-[1vh] px-[2vw] rounded-md"
+            onClick={()=>navigate('/habits')}>Habits</span>
+            <span className="hover:bg-[#1211112f] transition-all hover:-translate-y-1 py-[1vh] px-[2vw] rounded-md"
+            onClick={()=>navigate('/pomodoro')}>Pomodoro</span>
         </div>
     )
 }
