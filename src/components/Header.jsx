4b76975d1384f02bcb97bd4b81ae0d9c461d45d3
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-05 16:04:49 +0800

    created a function to check for current path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 export default function Header(){
+
+    const location = useLocation()
+    const [page, setPage] = useState('')
+
+    useEffect(()=>{
+        if(location.pathname==='/'){
+            setPage('habits')
+        }else{
+            setPage('pomodoro')
+        }
+    },[])
 
 const navigate = useNavigate()
 
     return(
         <div className="flex justify-evenly font-mono font-bold text-2xl py-[2vh] cursor-default
         ">
-            <span className="hover:bg-[#1211112f] transition-all hover:-translate-y-1 py-[1vh] px-[2vw] rounded-md"
-            onClick={()=>navigate('/habits')}>Habits</span>
-            <span className="hover:bg-[#1211112f] transition-all hover:-translate-y-1 py-[1vh] px-[2vw] rounded-md"
-            onClick={()=>navigate('/pomodoro')}>Pomodoro</span>
+            <span className={`${page==='habits' ? 'bg-[#1211112f]' : ''} hover:bg-[#1211112f] transition-all hover:-translate-y-1 py-[1vh] px-[2vw] rounded-md`}
+            onClick={()=>{navigate('/');setPage('habits')}}>Habits</span>
+
+            <span className={`${page==='pomodoro' ? 'bg-[#1211112f]' : ''} hover:bg-[#1211112f] transition-all hover:-translate-y-1 py-[1vh] px-[2vw] rounded-md`}
+            onClick={()=>{navigate('/pomodoro');setPage('pomodoro')}}>Pomodoro</span>
         </div>
     )
 }
