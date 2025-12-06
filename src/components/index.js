cf8bec2b3f025f47ea5f25f155667a40b137243e
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-06 15:11:24 +0800

    added exports for new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Header from './Header.jsx'
 import AddModal from './AddModal.jsx'
+import DeleteModal from './DeleteModal.jsx'
 
-export {Header, AddModal}
+export {Header, AddModal, DeleteModal}
