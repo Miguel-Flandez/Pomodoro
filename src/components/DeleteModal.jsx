77815bb927d9681072de566a5de875787b75197b
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-11 16:53:51 +0800

    added emit handler

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import {MyContext} from '@/context'
 
-export default function DeleteModal(){
+export default function DeleteModal({deleteHandler}){
 
     const {setDeleteChoice} = useContext(MyContext)
 
@@ -11,8 +11,8 @@ export default function DeleteModal(){
             <div className={` bg-blue-800 border-[#1211112f] flex flex-col justify-around absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-[1rem] px-[1.25rem]`}>
                 <span className="font-bold">Are you sure you want to delete this habit?</span>
                 <div id="choice" className="flex justify-between">
-                    <span className="bg-green-800 hover:bg-green-700" onClick={()=>setDeleteChoice(true)}>Yes</span>
-                    <span className="bg-red-800 hover:bg-red-700" onClick={()=>setDeleteChoice(false)}>No</span>
+                    <span className="bg-green-800 hover:bg-green-700" onClick={()=>{setDeleteChoice(true);deleteHandler(true)}}>Yes</span>
+                    <span className="bg-red-800 hover:bg-red-700" onClick={()=>{setDeleteChoice(false);deleteHandler(false)}}>No</span>
                 </div>
             </div>
         </div>
