1d6d7756c0137ed3c4edb551c050243764637cff
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-17 16:50:12 +0800

    removed font-mono utility classes

--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -83,9 +83,9 @@ export default function Pomodoro(){
       <>
       <Header/>
         <div className="flex flex-col gap-[5vw] items-center ">
-          <span className="block font-bold text-[2rem] text-white font-mono mt-4 text-center">Cycle {pomoTracker}</span>
+          <span className="block font-bold text-[2rem] text-white mt-4 text-center">Cycle {pomoTracker}</span>
           <div className={`${selected==='Pomodoro' ? 'bg-[#852D2D]' : selected==='Short' ? 'bg-[#225F63]' : 'bg-[#234F6D]'} 
-          rounded-md transition-colors duration-200 border-0 text-white text-md font-mono p-8 max-sm:w-[90%] flex flex-col items-center gap-[2rem]`}>   
+          rounded-md transition-colors duration-200 border-0 text-white text-md p-8 max-sm:w-[90%] flex flex-col items-center gap-[2rem]`}>   
 
               <div id="header buttons" className="flex items-center justify-center gap-[1vw] max-sm:text-xs cursor-pointer px-[1rem] whitespace-nowrap">
                   <span className={`px-4 py-2 rounded-sm ${selected==='Pomodoro' ? 'bg-[#1211112f]' : '' }`} onClick={()=>setSelected('Pomodoro')}>Pomodoro</span>
@@ -95,8 +95,8 @@ export default function Pomodoro(){
 
           
               <span className="text-[5rem] font-bold">{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</span>
-              <div id="button-container" className="h-16">
-                <button className={`bg-[#0E1011] rounded-md transition-colors duration-200 ${selected==='Pomodoro'?'text-[#C85757]' : selected==='Short' ? 'text-[#79CBD0]' : 'text-[#6EA8D0]'} font-bold font-mono px-12 py-4 ${start ? 'translate-y-1' : 'border-b-4 border-[#1A1D1F] '}`}
+              <div id="button-container" className="h-[4rem]">
+                <button className={`bg-[#0E1011] rounded-md transition-colors duration-200 ${selected==='Pomodoro'?'text-[#C85757]' : selected==='Short' ? 'text-[#79CBD0]' : 'text-[#6EA8D0]'} font-bold px-[3rem] py-[1rem] ${start ? 'translate-y-1' : 'border-b-4 border-[#1A1D1F] '}`}
                 onClick={()=>setStart(prev=>!prev, clickSound.play())}>{start?'PAUSE':'START'}</button>
               </div>
               
