eb9ece45da7fb36e847b3198778d3bd5d0076037
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-18 17:03:41 +0800

    improved hover animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,10 +19,10 @@ const navigate = useNavigate()
     return(
         <div className="flex justify-evenly font-mono font-bold text-2xl py-[2vh] cursor-default
         ">
-            <span className={`${page==='habits' ? 'bg-[#1211112f]' : ''} hover:bg-[#1211112f] transition-all hover:-translate-y-1 py-[1vh] px-[2vw] rounded-md`}
+            <span className={`${page==='habits' ? 'bg-[#1211112f]' : 'hover:-translate-y-1' } hover:bg-[#1211112f] transition-all  py-[1vh] px-[2vw] rounded-md`}
             onClick={()=>{navigate('/');setPage('habits')}}>Habits</span>
 
-            <span className={`${page==='pomodoro' ? 'bg-[#1211112f]' : ''} hover:bg-[#1211112f] transition-all hover:-translate-y-1 py-[1vh] px-[2vw] rounded-md`}
+            <span className={`${page==='pomodoro' ? 'bg-[#1211112f]' : 'hover:-translate-y-1' } hover:bg-[#1211112f] transition-all py-[1vh] px-[2vw] rounded-md`}
             onClick={()=>{navigate('/pomodoro');setPage('pomodoro')}}>Pomodoro</span>
         </div>
     )
