8e3fb32616dc8a41d822badbadbc572de4e49bc6
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-18 17:02:46 +0800

    added documentation

--- src/components/AddHabitModal.jsx
+++ src/components/AddHabitModal.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react"
 
-export default function AddHabitModal({addModalState, habitHandler, editValues, resetHandler}){
+export default function AddHabitModal({setShowAddModal, habitHandler, editValues, resetHandler}){
 
     const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     const inputRef = useRef(null)
@@ -8,7 +8,7 @@ export default function AddHabitModal({addModalState, habitHandler, editValues, 
     const [selectedDay, setSelectedDay] = useState([])
     const [habitName, setHabitName] = useState('')
 
-    
+    // inputs selected days of the week in the selected day array
     function toggleDay(day){
         setSelectedDay(prev=>{
             if(prev.includes(day)){
@@ -17,14 +17,17 @@ export default function AddHabitModal({addModalState, habitHandler, editValues, 
             return [...prev, day]
         })
     }
+
+    // empties and closes the modal
     function close(){
-        addModalState(false);
+        setShowAddModal(false);
         inputRef.current.value = ''
         setSelectedDay([]);
         setHabitName('')
         resetHandler(true)
     }
 
+    // prevents user from proceeding if there are no input
     function confirm(){
         if(inputRef.current.value && inputRef.current.value.trim() && selectedDay.length>0){
             close();
@@ -40,6 +43,7 @@ export default function AddHabitModal({addModalState, habitHandler, editValues, 
          
     }
 
+    // automatically inputs previous value of the habit in the modal for editing 
     useEffect(()=>{
         if(editValues){        
         inputRef.current.value = editValues.name
@@ -55,7 +59,7 @@ export default function AddHabitModal({addModalState, habitHandler, editValues, 
         <div id="modal-container"
         className="h-screen relative bg-[#144272] flex flex-col items-center py-[2rem] px-[1rem] gap-[5vw] rounded-r-4xl">
 
-            <div className="flex items-center justify-between gap-[2vw] w-[90%]">
+            <div id="add-prompt" className="flex items-center justify-between gap-[2vw] w-[90%]">
                 <span className="text-[2.5rem] font-bold whitespace-nowrap">Add a Habit</span>
                 <button onClick={close}>
                     <i className="fa-solid fa-xmark transition-transform hover:rotate-90 text-[2rem]"></i>
@@ -65,15 +69,15 @@ export default function AddHabitModal({addModalState, habitHandler, editValues, 
 
             
 
-            <div id="add" className="flex flex-col w-[90%]">
+            <div id="habit-name" className="flex flex-col w-[90%]">
                 <label htmlFor="habit" className="text-[2rem]">Name this Habit</label>
                 <input ref={inputRef} type="text" id="habit" className="text-[2rem] border-b-2 focus:outline-none h-[3rem]" autoComplete="off" onChange={(e)=>{setHabitName(e.target.value)}}/>
                 
             </div>
 
-            <div id="frequency" className="w-[90%] grid grid-cols-[repeat(4,1fr)] gap-[2vw] justify-evenly flex-wrap ">
+            <div id="days-container" className="w-[90%] grid grid-cols-[repeat(4,1fr)] gap-[2vw] justify-evenly flex-wrap ">
                 {days.map(day=>(
-                    <button key={day} className={`${selectedDay.find(e=>e===day)?'bg-[#2C74B3]! border-[#2C74B3]!':''} transition-all text-[1rem]  bg-[#1211112f] hover:bg-[#205295] rounded-md px-[1rem] py-[0.25rem]`}
+                    <button key={day} className={`${selectedDay.find(e=>e===day)&&'bg-[#2C74B3]! border-[#2C74B3]!'} transition-all text-[1rem]  bg-[#1211112f] hover:bg-[#205295] rounded-md px-[1rem] py-[0.25rem]`}
                     onClick={()=>toggleDay(day)}>{day}</button>
                 ))}
             </div>
@@ -83,7 +87,7 @@ export default function AddHabitModal({addModalState, habitHandler, editValues, 
                 onClick={confirm}>Confirm</button>
             </div>
 
-            <span className="absolute bottom-3 left-1/2 -translate-x-1/2 whitespace-nowrap">{warning}</span>
+            <span id="warning" className="absolute bottom-3 left-1/2 -translate-x-1/2 whitespace-nowrap">{warning}</span>
 
         </div>
     )
